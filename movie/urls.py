0c1043a9867e3299349f5c1ce61cecf36e2e7cbc
fuatsevinc <fuatsevinc@icloud.com>
2022-10-17 11:57:05 +0200

    project updated

--- movie/urls.py
+++ movie/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("movie", views.movie),
+    path("", views.index, name="index"),
+    path("movie", views.movie, name="movie"),
     path("movie/<slug:slug>", views.movie_detail),
     
     
